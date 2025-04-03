8d12d904e30910fd8a129af22f1a50f387844172
anup kushwaha <anupk6347@gmail.com>
2025-04-03 23:21:26 +0530

    correction

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const mongoose = require ("mongoose");
 require ("dotenv").config();
 exports.dbConnect=()=>{
    mongoose.connect(process.env.MONGODB_URL,{
-    //  useNewUrlParser: true,
-    //  useUnifiedTopology: true, 
+     useNewUrlParser: true,
+     useUnifiedTopology: true, 
    })
    .then(console.log("db connection Successful"))
    .catch((err)=>{
